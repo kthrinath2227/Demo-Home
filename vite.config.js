d5b610ffc40577e8f2079857a83460ab8d376714
kthrinath2227 <k.thrinath2227@gmail.com>
2025-10-07 22:01:40 +0530

    Add gh-pages deploy setup and update vite config

--- vite.config.js
+++ vite.config.js
@@ -202,6 +202,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
+	 base: "/Demo-Home/",
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin(), iframeRouteRestorationPlugin()] : []),
